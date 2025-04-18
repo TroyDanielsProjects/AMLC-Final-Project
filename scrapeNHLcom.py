31611c7164eb391ca5ae74ffbc2ed2e3273caf13
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-04-18 12:58:10 -0400

    Added NHL insider data

--- scrapeNHLcom.py
+++ scrapeNHLcom.py
@@ -3,6 +3,8 @@ import requests
 import re
 
 class Parser():
+
+    insider_ref_dict = {}
     
     def get_html_from_url(self, url):
         try:
@@ -21,9 +23,9 @@ class Parser():
                 month, days_in_month = months[i]
                 for day in range(4, days_in_month+1):
                     url = f"https://www.nhl.com/news/nhl-buzz-news-and-notes-{month}-{day}-{year}"
-                    if url:
-                        ofile.write(f"{month}-{day}-{year}:\n")
-                        html = self.get_html_from_url(url)
+                    ofile.write(f"{month}-{day}-{year}:\n")
+                    html = self.get_html_from_url(url)
+                    if html:
                         soup = BeautifulSoup(html, "html.parser")
                         text_div = soup.find_all('div', class_="oc-c-markdown-stories")
                         for match in text_div:
@@ -33,7 +35,7 @@ class Parser():
                             ofile.write(text)
                     else:
                         continue
-
+    
     def clean_text(self, path):
         with open(path, 'r') as rfile:
             with open(path+"_cleaned", 'w') as wfile:
@@ -45,10 +47,42 @@ class Parser():
                         wfile.write(line)
                     previous_line = line
 
+    def get_home_page_links(self, base_url="https://www.nhl.com", path="/news", depth=0, max_depth=100):
+        print(depth)
+        url = base_url + path
+        html = self.get_html_from_url(url)
+        if html:
+            soup = BeautifulSoup(html, "html.parser")
+            for link in soup.find_all('a'):
+                ref = link.get('href')
+                if ref and ref.startswith("/news/topic/nhl-insider/"):
+                    if ref not in self.insider_ref_dict:
+                        self.insider_ref_dict[ref] = 1
+                        if depth < max_depth:
+                            self.get_home_page_links(path=ref, depth=depth+1)
 
-                
-                
+    def write_out_insider_dict(self):
+        with open("data/insiderrefs.txt", 'w') as ofile:
+            for key in self.insider_ref_dict:
+                ofile.write(key+"\n")
+                    
+    def get_insider_text(self):
+        with open("./data/nhlcominsidertext.txt", 'w') as ofile:
+                for ref in self.insider_ref_dict:
+                    url = f"https://www.nhl.com{ref}"
+                    html = self.get_html_from_url(url)
+                    if html:
+                        soup = BeautifulSoup(html, "html.parser")
+                        text_div = soup.find_all('div', class_="oc-c-markdown-stories")
+                        for match in text_div:
+                            text = match.get_text()
+                            ofile.write(text)
+                    else:
+                        continue
+
 
 parser = Parser()
-parser.iterate_over_year("2025")
+parser.get_home_page_links()
+# parser.get_insider_text()
+
 
