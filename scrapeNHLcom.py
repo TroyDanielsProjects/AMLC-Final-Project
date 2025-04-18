d8b62de0ed6e601badb8dabde3d5cf4fd95795fc
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-04-18 09:56:33 -0400

    Added base webscraper for NHL.com and small dataset

---
+++ scrapeNHLcom.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+class Parser():
+    
+    def get_html_from_url(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.text
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None   
+        
+    def iterate_over_year(self, year):
+        months = [("January", 31), ("February", 28), ("March", 31), ("April", 30), ("May", 31), ("June", 30), ("July", 31), ("August", 31),
+                   ("September", 30), ("October", 31), ("November", 30), ("December", 31)]
+        with open("./data/nhlcombuzznewsnotes.txt", 'a') as ofile:
+            for i in range(len(months)):
+                month, days_in_month = months[i]
+                for day in range(4, days_in_month+1):
+                    url = f"https://www.nhl.com/news/nhl-buzz-news-and-notes-{month}-{day}-{year}"
+                    if url:
+                        ofile.write(f"{month}-{day}-{year}:\n")
+                        html = self.get_html_from_url(url)
+                        soup = BeautifulSoup(html, "html.parser")
+                        text_div = soup.find_all('div', class_="oc-c-markdown-stories")
+                        for match in text_div:
+                            text = match.get_text()
+                            str_to_remove = "Welcome to the NHL Buzz. Each day during the regular season, NHL.com has you covered with all the latest news.\n"
+                            text = text.replace(str_to_remove, "")
+                            ofile.write(text)
+                    else:
+                        continue
+
+    def clean_text(self, path):
+        with open(path, 'r') as rfile:
+            with open(path+"_cleaned", 'w') as wfile:
+                previous_line = ""
+                for line in rfile:
+                    if previous_line == line:
+                        continue
+                    else:
+                        wfile.write(line)
+                    previous_line = line
+
+
+                
+                
+
+parser = Parser()
+parser.iterate_over_year("2025")
+
