9327a2d131d7e26b0f4bf4cc9ed70207aab02984
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-04-29 09:29:42 -0400

    Another quick fix

--- finetune.py
+++ finetune.py
@@ -278,9 +278,10 @@ class Trainer:
             print(f"Failed to load model: {e}")
         if model is None:
             #quantization_config=self.quantization_config
-            quant_model = AutoModelForCausalLM.from_pretrained("google/gemma-2b", quantization_config=self.quantization_config,torch_dtype=torch.float16).to(self.device)
+            quant_model = AutoModelForCausalLM.from_pretrained("google/gemma-2b", quantization_config=self.quantization_config,torch_dtype=torch.float16)
             quant_model.gradient_checkpointing_enable()
             model = get_peft_model(quant_model, self.lora_config)
+            model = model.to(self.device)
             print("Loading new model")
         return model
     
