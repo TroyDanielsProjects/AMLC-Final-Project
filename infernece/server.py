aac0b4e03ff77f0f4a064d6dd275cf4dc7e21c47
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-05-01 16:19:05 -0400

    Update server.py

--- infernece/server.py
+++ infernece/server.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     load_dotenv()
     access_token = os.getenv("HF_TOKEN")
     login(token=access_token)
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=80)
